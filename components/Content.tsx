53d779a93f1ba6f0a912fe3841cdef82f4a6eda6
Rasheed Johnson <rasheedjohnson@tutanota.com>
2024-05-12 05:17:52 +0200

    first commit, add psych details to the site. to be updated later

---
+++ components/Content.tsx
@@ -0,0 +1,32 @@
+import { promises as fs } from 'fs';
+import items from "@/app/lib/chapter2.json";
+
+export default async function Content() {
+  // const itemsList = items.sort(())
+  const title_style = 'font-semibold text-xl mb-5 sm:text-lg md:text-xl'
+  const desc_style = 'font-light opacity-75 text-sm md:text-md'
+
+  const info_style = 'flex flex-col w-[200px] md:w-[300px] lg:w-[450px] xl:w-[600px] p-6 border-slate-300/5 hover:scale-[105%] transition-all'
+
+  return (
+    <div className=''>
+      { 
+        items.map((item) => (
+          <div key={item.id} className='flex flex-col justify-center items-center'>
+            <div className='flex flex-row justify-center items-start gap-5 list-none z-10 m-5'>
+              <div className={info_style}>
+                <p key={item.title.length} className={title_style}>{item.title.toLocaleUpperCase()}</p>
+                <p key={item.description.length} className={desc_style}>{item.description}</p>
+              </div>
+              <div  className={info_style}>
+                <p key={item.title.length} className={title_style}>{item.german_title.toLocaleUpperCase()}</p>
+                <p key={item.description.length} className={desc_style}>{item.german_description}</p>
+              </div>
+            </div>
+            <div className='w-[100px] md:w-[500px] lg:w-[800px] xl:w-[1200px] h-[1px] bg-gradient-to-r from-slate-500 from-10% via-slate-900 via-30% to-slate-500 to-90%'></div>
+          </div>
+        ))
+      }
+    </div>
+  );
+}
