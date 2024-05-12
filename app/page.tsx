449c78a37d8a25b731dbb8f9c5a22258b02c377e
Rasheed Johnson <rasheedjohnson@tutanota.com>
2024-05-12 05:39:26 +0200

    update to better fit mobile screen width

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,6 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col justify-center items-center">
         {/* GRADIENT */}
-        <div className="w-[350px] h-[150px] bg-gradient-to-br from-slate-600 to-black bg-slate-950 rounded-b-full rounded-tr-2xl blur-lg absolute opacity-20"></div>
-        <div className="w-[100px] h-[250px] bg-slate-950 rounded-full blur-lg absolute opacity-30"></div>
-
 
         <h1 className="font-medium text-3xl mb-4">Psychology Database</h1>
         <div className='w-[400px] h-[1px] mb-10 bg-gradient-to-r from-black from-10% via-slate-700 via-30% to-black to-90%'></div>
--- components/Content.tsx
+++ components/Content.tsx
@@ -3,13 +3,14 @@ import items from "@/app/lib/chapter2.json";
 
 export default async function Content() {
   // const itemsList = items.sort(())
-  const title_style = 'font-semibold text-xl mb-5 sm:text-lg md:text-xl'
-  const desc_style = 'font-light opacity-75 text-sm md:text-md'
+  const title_style = 'font-medium mb-5 text-md md:text-xl'
+  const desc_style = 'font-light opacity-75 text-xs md:text-md'
 
   const info_style = 'flex flex-col w-[200px] md:w-[300px] lg:w-[450px] xl:w-[600px] p-6 border-slate-300/5 hover:scale-[105%] transition-all'
 
   return (
-    <div className=''>
+    <div className='flex flex-col justify-center items-center'>
+      <h3 className='text-xl opacity-75'>II: Neural and Hormonal Systems</h3>
       { 
         items.map((item) => (
           <div key={item.id} className='flex flex-col justify-center items-center'>
