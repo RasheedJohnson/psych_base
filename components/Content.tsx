0b1ea45f4b2c72b8dea7a7d9f7ac5de7822e879a
Rasheed Johnson <rasheedjohnson@tutanota.com>
2024-05-15 01:56:18 +0200

    fix x-overflow visibility and add clear chapter titles

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,46 +1,58 @@
 import { promises as fs } from 'fs';
-import items from "@/app/lib/chapter2.json";
+import items from "@/app/lib/new_test.json";
 
 export default async function Content() {
-  // const itemsList = items.sort(())
+  
+  const content_container_styles = 'flex flex-col justify-center items-center';
+  const content_styles = 'flex flex-row justify-center items-start gap-5 list-none z-10 m-5';
+
+  const chapter_container_styles = 'flex flex-col justify-center items-center mt-5 ';
+  const chapter_styles = 'text-md opacity-70 font-light'
+  const chapter_number_styles = 'text-white opacity-100 text-lg font-semibold'
+
   const title_style = 'font-medium mb-5 text-md md:text-xl'
   const desc_style = 'font-light opacity-75 text-xs md:text-md'
 
   const info_style = 'flex flex-col w-[200px] md:w-[300px] lg:w-[450px] xl:w-[600px] p-6 border-slate-300/5 hover:scale-[105%] transition-all'
+  const horizontal_divider = 'w-[100px] md:w-[500px] lg:w-[800px] xl:w-[1200px] h-[1px] bg-gradient-to-r from-slate-500 from-10% via-slate-900 via-30% to-slate-500 to-90%';
   const len_match = 4;
+
+  const test_text = "chapter";
 
   return (
     <div className='flex px-4 flex-col justify-center items-center'>
-      { 
+      {
         items.map((item) => (
-
-          item.title == "CHAPTER" ? (
-            <div key={item.id} className="flex flex-col justify-center items-center mt-5">
-              <h3 className='text-xl opacity-75'>{item.title}: {item.description}</h3>
+          item.title.toLocaleLowerCase().includes(test_text) ? (
+            <div key={item.id} className={`${chapter_container_styles}`}>
+              <p className={chapter_number_styles}>{item.title}</p>
+              <h4 className={chapter_styles}>
+                {item.content}</h4>
             </div>
           ) : (
-            <div key={item.id} className='flex flex-col justify-center items-center'>
-              <div className='flex flex-row justify-center items-start gap-5 list-none z-10 m-5'>
+            <div key={item.id} className={content_container_styles}>
+              <div className={content_styles}>
                 <div className={info_style}>
-                  <p key={item.title.length} className={title_style}>
-                    {item.title.toLocaleUpperCase()}</p>
-                  <p key={item.description.length} className={desc_style}>
-                    {item.description}</p>
+                  <p className={title_style}>
+                    {item.title}</p>
+                  <p className={desc_style}>
+                    {item.content}</p>
                 </div>
-                  
-                  
+              
+              
                 <div className={info_style}>
-                  <p key={item.title.length} className={title_style}>
-                    {item.german_title.toLocaleUpperCase()}</p>
-                  <p key={item.description.length} className={desc_style}>
-                    {item.german_description}</p>
+                  <p className={title_style}>
+                    {item.de_title}</p>
+                  <p className={desc_style}>
+                    {item.de_content}</p>
                 </div>
               </div>
-              <div className='w-[100px] md:w-[500px] lg:w-[800px] xl:w-[1200px] h-[1px] bg-gradient-to-r from-slate-500 from-10% via-slate-900 via-30% to-slate-500 to-90%'>
+              <div className={horizontal_divider}>
               </div>
             </div>
+            )
           )
-        ))
+        )
       }
     </div>
   );
